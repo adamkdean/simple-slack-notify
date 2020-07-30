7fa0c209eeb0a1abef3f632694ed34e276011bd5
Adam K Dean <adamkdean@googlemail.com>
2020-07-30 16:51:37 +0100

    add basic slack support

--- src/index.js
+++ src/index.js
@@ -6,7 +6,50 @@
 // Slack notification action that doesn't suck
 // Copyright 2020 Adam K Dean <adamkdean@googlemail.com>
 
-// const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core')
+const { IncomingWebhook } = require('@slack/webhook')
 
-console.log(github.context)
+try {
+  const slack = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+
+  const channel = core.getInput('channel')
+  const username = core.getInput('username')
+  const iconEmoji = core.getInput('icon_emoji')
+  const iconUrl = core.getInput('icon_url')
+  const status = core.getInput('status')
+  const successText = core.getInput('success_text')
+  const failureText = core.getInput('failure_text')
+  const cancelledText = core.getInput('cancelled_text')
+
+  let color = core.getInput('color')
+  let text = core.getInput('text')
+  let fields = []
+
+  // If color isn't set but status is, infer the color
+  if (!color && status === 'success') color = 'good'
+  else if (!color && status === 'failure') color = 'danger'
+  else if (!color && status === 'cancelled') color = 'warning'
+  else color = '#eeeeee'
+
+  // If text isn't set, check for status specific text
+  if (!text && status === 'success' && successText) text = successText
+  else if (!text && status === 'failure' && failureText) text = failureText
+  else if (!text && status === 'cancelled' && cancelledText) text = cancelledText
+
+  // Send the notification
+  (async () => {
+    await slack.send({
+      text,
+      channel,
+      username,
+      icon_emoji: iconEmoji,
+      icon_url: iconUrl,
+      attachments: [{
+        color,
+        fields
+      }]
+    })
+  })()
+} catch (error) {
+  core.setFailed(error.message)
+}
