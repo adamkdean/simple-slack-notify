761686c4b726cf0d99093f255125a846a6e12664
Adam K Dean <adamkdean@googlemail.com>
2020-07-30 13:42:33 +0100

    test github context output

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+//  ___ _         _     _  _     _   _  __
+// / __| |__ _ __| |__ | \| |___| |_(_)/ _|_  _
+// \__ \ / _` / _| / / | .` / _ \  _| |  _| || |
+// |___/_\__,_\__|_\_\ |_|\_\___/\__|_|_|  \_, |
+//                                         |__/
+// Slack notification action that doesn't suck
+// Copyright 2020 Adam K Dean <adamkdean@googlemail.com>
+
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+console.log(github.context)
